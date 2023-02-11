8e8d47631b42fba668ca30abcd78e87168bc2316
kicu-xxo <kicu.xxo@gmail.com>
2023-02-11 22:14:37 +0900

    pagination first commit

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { ClipLoader } from "react-spinners";
 import MoviePageCards from "../components/MoviesPageCards";
+import Pagination from "../components/Pagination";
 
 const Movies = () => {
   const { popularMovies, loading3 } = useSelector((state) => state.movie);
   console.log(popularMovies);
+  const [limit, setLimit] = useState(10); //페이지 당 보여줄 게시물 수
+  const [page, setPage] = useState(1); //현재 페이지 번호
+  const offset = (page - 1) * limit; //페이지 당 첫 게시물 위치 계산
 
   // if (loading3) {
   //   return (
@@ -21,20 +25,28 @@ const Movies = () => {
   //   );
   // }
   return (
-    <div className="movies-page">
-      <div className="select-container">
-        <select>
-          <option>옵션 1</option>
-        </select>
-        <select>
-          <option>옵션 2</option>
-        </select>
+    <div>
+      <div className="movies-page">
+        <div className="select-container">
+          <select>
+            <option>옵션 1</option>
+          </select>
+          <select>
+            <option>옵션 2</option>
+          </select>
+        </div>
+          <ul className="movies-container">
+            {popularMovies.results.slice(offset, offset + limit).map((item) => (
+              <MoviePageCards item={item} key={item.id} />
+            ))}
+          </ul>
       </div>
-      <ul className="movies-container">
-        {popularMovies.results.map((item) => (
-          <MoviePageCards item={item} key={item.id} />
-        ))}
-      </ul>
+      <Pagination
+        total={popularMovies.results.length}
+        limit={limit}
+        page={page}
+        setPage={setPage}
+      />
     </div>
   );
 };
